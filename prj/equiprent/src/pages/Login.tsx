20b6eeb4d8ddf6804be190b56868ee468b07fae7
Vincenzo Bonura <vicio.bonura@gmail.com>
2025-01-05 20:36:22 +0100

    feat equipRent: update login system

--- prj/equiprent/src/pages/Login.tsx
+++ prj/equiprent/src/pages/Login.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { isLoggedIn } from '../utils/auth';
 import LoginForm from '../components/LoginForm/LoginForm';
 
 const Login = () => {
+    // Controllo utente già loggato
+    // XXX: centralizzare usando ProtectedRoute
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (isLoggedIn()) {
+            navigate('/');
+        }
+    }, [navigate]); 
+
     return (
         <div className="center-content">
             <LoginForm />
