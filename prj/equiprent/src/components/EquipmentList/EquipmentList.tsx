d142b499cffea640681335d4125ecbf01d6df590
Vincenzo Bonura <vicio.bonura@gmail.com>
2025-01-18 18:44:22 +0100

    feat equipRent: add featured equipment

--- prj/equiprent/src/components/EquipmentList/EquipmentList.tsx
+++ prj/equiprent/src/components/EquipmentList/EquipmentList.tsx
@@ -22,10 +22,28 @@ const EquipmentList = () => {
         return <div>Loading...</div>;
     }
 
+    const [featured, ...rest] = equipments;
+
     return <div className="equipments-list">
-        {
-            equipments.map((eq: Equipment) => (
-                <Card key={eq.id}>
+        <div className="equipments-list__featured">
+            <Card key={featured.id}>
+                <Card.Header>
+                    <h3><div dangerouslySetInnerHTML={{ __html: formatSVG(featured.icon) }} /> {featured.name}</h3>
+                </Card.Header>
+                <Card.Body>
+                    <img src="https://placehold.co/600x400" alt={featured.name} />
+                    <p>{featured.claim}</p>
+                    <div>{formatPrice(featured.pricePerMinute, 'EUR')}/min</div>
+                </Card.Body>
+                <Card.Footer>
+                    <BookingWidget equipment={featured} />
+                </Card.Footer>
+            </Card>
+        </div>
+        <div className="equipments-list__rest">
+            {
+                rest.map((eq: Equipment) => (
+                    <Card key={eq.id}>
                     <Card.Header>
                         <h3><div dangerouslySetInnerHTML={{ __html: formatSVG(eq.icon) }} /> {eq.name}</h3>
                     </Card.Header>
@@ -37,9 +55,10 @@ const EquipmentList = () => {
                     <Card.Footer>
                         <BookingWidget equipment={eq} />
                     </Card.Footer>
-                </Card>
-            ))
-        }
+                    </Card>
+                ))
+            }
+        </div>
     </div>;
 }
 
